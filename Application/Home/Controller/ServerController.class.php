65e00277d61adab624e009f5d82d26eb8d6c1b72
zhangyechi <zhangyechi11>
2017-12-15 17:59:09 +0800

    123

---
+++ Application/Home/Controller/ServerController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+include_once vendor/autoload.php;
+
+class ServerController extends Controller {
+    public function index()
+    {
+        echo 123;
+    }
+}
