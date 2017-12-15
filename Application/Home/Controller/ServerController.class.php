b4ab2c4bee311f735999384f704abfdf6ee01cc1
zhangyechi <zhangyechi11>
2017-12-15 18:05:12 +0800

    123

--- Application/Home/Controller/ServerController.class.php
+++ Application/Home/Controller/ServerController.class.php
@@ -2,10 +2,28 @@
 namespace Home\Controller;
 use Think\Controller;
 include_once vendor/autoload.php;
+use EasyWeChat\Factory;
 
 class ServerController extends Controller {
     public function index()
     {
-        echo 123;
+        $config = [
+            'app_id' => C('AppID'),
+            'secret' => C('appsecret'),
+
+            'response_type' => 'array',
+
+            'log' => [
+                'level' => 'debug',
+                'file' => __DIR__.'/wechat.log',
+            ],
+        ];
+
+        $app = Factory::officialAccount($config);
+
+        $response = $app->server->serve();
+
+// 将响应输出
+        $response->send(); // Laravel 里请使用：return $response;
     }
 }
